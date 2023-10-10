a728762c71f5d3157afd938496e1aaf3d5db5600
soken <nsongkien@gmail.com>
2023-10-10 11:20:03 +0300

    improved data structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,56 @@ import './App.css'
 import NavMain from './components/navigation/NavMain'
 
 function App() {
-  const [user,setUser] = useState({id:1})
-  const chats = [
+  const [user,setUser] = useState({
+      id:1,
+      name:'cat',
+      avatar:'1',
+      chats:[0,1]
+  })
+  const [userChats,setUserChats] = useState([
     {
       id:0,
-      name:'Self',
-      members: [1],
-      read:true,
+      createdBy:1,
+      members:[1],
+      name:'self',
       messages:[
-        {
-          content:'hehe',
-        }
-      ]
+          {
+            content:'first self message',
+            sentBy:1,
+            readBy:[1]
+          },
+          {
+            content:'second self message',
+            sentBy:1,
+            readBy:[1]
+          }
+        ]
     },
     {
       id:1,
-      name:'Weirdo',
-      members: [2,1],
-      read:true,
+      createdBy:2,
+      members:[1,2],
+      name:'__PEER__',
       messages:[
-        {
-          content:'hehe',
-        }
+          {
+            content:'first peer message',
+            sentBy:2,
+            readBy:[1],
+          },
+          {
+            content:'second peer message',
+            sentBy:1,
+            readBy:[2],
+          }
       ]
-    },
-  ]
+    }
+  ])
+  
   const [activeChat,setActiveChat] = useState(1)
   return (
     <>
-      <Header user={user} chat={chats.find(chat=>chat.id===activeChat)}/>
-      <NavMain activeChat={activeChat} chats={chats}/>
+      <Header user={user} chat={userChats.find(chat=>chat.id===activeChat)}/>
+      <NavMain activeChat={activeChat} chats={userChats}/>
     </>
   )
 }
